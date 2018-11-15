45fe2b1069a6af1bbb4287a0dfc8bc448acf4749
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-11-15 10:48:37 +0800

    new ci solution

--- build.py
+++ build.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+from conanos.build import Main
 
-
-from bincrafters import build_template_default
-import os
-
-os.environ['CONAN_USERNAME'] = os.environ.get('CONAN_USERNAME','conanos')
-
-if __name__ == "__main__":
-
-    builder = build_template_default.get_builder()
-
-    builder.run()
+if __name__ == "__main__":    
+    Main('expat',pure_c=True)
